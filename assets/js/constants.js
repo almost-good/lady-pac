128feb2b7591775b0ab6959411ead3f151869d4d
almost-good <marja.marinic@gmail.com>
2023-01-28 12:04:16 +0000

    feat[constants]: add constants for local storage

---
+++ assets/js/constants.js
@@ -0,0 +1,5 @@
+// Constant used for local storage, stores a list of top 10 scores and it's player names
+export const SCORES = "scores";
+
+// Constant used for local storage, stores current player name
+export const PLAYER = "player";
