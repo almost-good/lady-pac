b7ac037c7a8a74c6c623127c7e3c6c2ddf14e2e1
almost-good <marja.marinic@gmail.com>
2023-01-28 15:11:27 +0000

    feat[leaderboard]: store current score in local storage at appropriate postion

--- assets/js/leaderboard.js
+++ assets/js/leaderboard.js
@@ -12,18 +12,69 @@ export default class Leaderboard {
     // Add current player to local storage
     localStorage.setItem(PLAYER, this.currentPlayerScore.player);
 
-    // check if local storage is empty
-    if (!localStorage.getItem(SCORES)) {
-      this.#storeInitialCurrentScore();
-    }
+    let scoreTest = [
+      { player: "a", score: "2000" },
+      { player: "b", score: "1900" },
+      { player: "c", score: "1800" },
+      { player: "d", score: "1700" },
+      { player: "e", score: "1600" },
+      { player: "f", score: "1590" },
+      { player: "g", score: "1580" },
+      { player: "h", score: "1570" },
+      { player: "i", score: "1560" },
+      { player: "j", score: "1550" },
+    ];
+    localStorage.setItem(SCORES, JSON.stringify(scoreTest));
+
+    this.#storeCurrentScore();
   }
 
   /**
-   * Store the first score ever achieved in local storage.
+   * Store current score of the player in the leaderboard, if applicable.
+   * @summary
+   * The leaderboard only stores top 10 scores achieved.
    */
-  #storeInitialCurrentScore() {
-    let leaderboardStorage = [this.currentPlayerScore];
 
+  #storeCurrentScore() {
+    let leaderboardStorage = JSON.parse(localStorage.getItem(SCORES));
+
+    // Check if leaderbord contains a value.
+    if (leaderboardStorage) {
+      // Get current player score positon.
+      let position = this.#leaderboardPosition(leaderboardStorage);
+      leaderboardStorage.splice(position, 0, this.currentPlayerScore);
+    } else {
+      // Add a value
+      leaderboardStorage = [this.currentPlayerScore];
+    }
+
+    this.#addToLocalStorage(leaderboardStorage);
+  }
+
+  /**
+   * Find position of where current score should be placed in the leaderboard.
+   * @param {[]} leaderboardStorage
+   * - Array of player names and their scores.
+   * @return {number} Position of the current player score.
+   */
+
+  #leaderboardPosition(leaderboardStorage) {
+    for (let i = leaderboardStorage.length - 1; i >= 0; i--) {
+      if (this.currentPlayerScore.score < leaderboardStorage[i].score) {
+        return i + 1;
+      }
+    }
+
+    return 0;
+  }
+
+  /**
+   * Add entire leaderboard to local storage.
+   * @param {[]} leaderboardStorage
+   * - Contains an array of player names and their scores.
+   */
+
+  #addToLocalStorage(leaderboardStorage) {
     localStorage.setItem(SCORES, JSON.stringify(leaderboardStorage));
   }
 }
