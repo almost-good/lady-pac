670d1a31113c0925f9ffd2f06cdb233d35d9fd01
almost-good <marja.marinic@gmail.com>
2023-02-02 20:17:06 +0000

    feat[game, map]: connect game and map classes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,16 +1,22 @@
+import GameMap from "./map.js"
+
 export default class Game {
   constructor() {
+    this.squareSize = 20; 
+    
+    // Canvas
     this.canvas = document.getElementById('game-canvas')
     this.ctx = this.canvas.getContext('2d')
+
+    // Create new Map object.
+    this.gameMap = new GameMap(this.squareSize);
   }
-
+  
   game() {
-    this.#runGame()
-
-    setInterval(this.#runGame, 1000)
+    setInterval(this.#runGame.bind(this), 1000)
   }
 
   #runGame() {
-    console.log('run')
+    this.gameMap.create()
   }
 }
---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+export default class GameMap {
+  constructor(squareSize) {
+    this.squareSize = squareSize
+  }
+
+  create() {
+    console.log(this.squareSize)
+  }
+}
