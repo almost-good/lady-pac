a48a4dab211f4606ec2280b21cacbde67a184de2
almost-good <marja.marinic@gmail.com>
2023-02-03 11:03:00 +0000

    docs[game]: add docstring for #runGame and game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,7 +2,7 @@ import GameMap from "./map.js";
 
 export default class Game {
   constructor() {
-    this.squareSize = 20;
+    this.squareSize = 40;
 
     // Canvas
     this.canvas = document.getElementById("game-canvas");
@@ -12,13 +12,27 @@ export default class Game {
     this.gameMap = new GameMap(this.squareSize);
   }
 
+  /** 
+  * Call the game.
+  * @summary 
+  * Allows us to start the game. 
+  * This function contains all setup needed for game to run.
+  */
+  
   game() {
     // Set the size of canvas.
     this.gameMap.setCanvasSize(this.canvas);
+
+    // Run the game once every second.
     setInterval(this.#runGame.bind(this), 1000);
   }
 
+  /** 
+  * Run the game.
+  * @summary Main function which runs one instance of the game.
+  */
+  
   #runGame() {
-    this.gameMap.create();
+    this.gameMap.create(this.ctx);
   }
 }
