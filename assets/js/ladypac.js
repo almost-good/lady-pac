5a6431f1d6e10dd065be6f30a9ccc07ffe382044
almost-good <marja.marinic@gmail.com>
2023-02-03 19:46:28 +0000

    feat[ladypac]: detect which key control was pressed

--- assets/js/ladypac.js
+++ assets/js/ladypac.js
@@ -1,3 +1,5 @@
+import { MoveDirection } from "./constants.js";
+
 /**
 * Lady Pac object.
 * 
@@ -18,6 +20,12 @@ export default class LadyPac {
     this.row = row;
     this.gameMap = gameMap;
     
+    // Moving directions
+    this.moveDirection = null
+    this.checkMoveDirection = null
+
+    document.addEventListener('keydown', this.#checkKeyPressedEvent)
+
     this.#getImages()
   }
 
@@ -32,6 +40,29 @@ export default class LadyPac {
     this.yPosition = this.row * squareSize;
 
     ctx.drawImage(this.ladyPacImgs[this.ladyPacImgIndex], this.xPosition, this.yPosition, squareSize, squareSize)
+  }
+
+  /**
+   * Check which key was pressed.
+   */
+
+  #checkKeyPressedEvent = (event) => {
+    // Up key.
+    if(event.keyCode === 38) {
+      console.log('up')
+    }
+    // Down key.
+    if(event.keyCode === 40) {
+      console.log('down')
+    }
+    // Left key.
+    if(event.keyCode === 37) {
+      console.log('left')
+    }
+    // Right key.
+    if(event.keyCode === 39) {
+      console.log('right')
+    }
   }
 
   /**
