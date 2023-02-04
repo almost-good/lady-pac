22baca31ff9b6b306e27b6ba9c5e286b454946fe
almost-good <marja.marinic@gmail.com>
2023-02-04 09:49:50 +0000

    feat[constants]: add move direction constant

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,5 +1,13 @@
-// Constant used for local storage, stores a list of top 10 scores and it's player names
+// Constant used for local storage, stores a list of top 10 scores and it's player names.
 export const SCORES = "scores";
 
-// Constant used for local storage, stores current player name
+// Constant used for local storage, stores current player name.
 export const PLAYER = "player";
+
+// Constant used for moving direction.
+export const MoveDirection = {
+  up: 0,
+  down: 1,
+  left: 2,
+  right: 3
+}
