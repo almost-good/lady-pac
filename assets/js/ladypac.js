e1c0415fb9aaf1a7db78816dc39fdb2b9f784279
almost-good <marja.marinic@gmail.com>
2023-02-04 10:04:37 +0000

    feat[ladypac]: check which key was pressed

--- assets/js/ladypac.js
+++ assets/js/ladypac.js
@@ -1,32 +1,43 @@
 import { MoveDirection } from "./constants.js";
 
 /**
-* Lady Pac object.
-* 
-* Conatins all information about Lady Pac.
-*
-* Public methods:
-*
-*     create(ctx, squareSize)
-*
-* Private methods:
-*
-*     #getImages()
-*/
+ * Lady Pac object.
+ *
+ * Conatins all information about Lady Pac.
+ *
+ * Public methods:
+ *
+ *     create(ctx, squareSize)
+ *
+ * Event methods:
+ *
+ *     #checkKeyPressedEvent
+ *
+ * Private methods:
+ *
+ *     #getImages()
+ */
 
 export default class LadyPac {
-  constructor(column, row, gameMap) {
+  constructor(speed, column, row, gameMap) {
+    this.speed = speed;
+
     this.column = column;
     this.row = row;
     this.gameMap = gameMap;
-    
+
     // Moving directions
-    this.moveDirection = null
-    this.checkMoveDirection = null
+    this.moveDirection = null;
+    this.checkMoveDirection = null;
 
-    document.addEventListener('keydown', this.#checkKeyPressedEvent)
+    // How much Lady Pac moved
+    this.xMoveSteps = 0;
+    this.yMoveSteps = 0;
 
-    this.#getImages()
+    // Event listeners
+    document.addEventListener("keydown", this.#checkKeyPressedEvent);
+
+    this.#getImages();
   }
 
   /**
@@ -36,34 +47,60 @@ export default class LadyPac {
    */
 
   create(ctx, squareSize) {
-    this.xPosition = this.column * squareSize;
-    this.yPosition = this.row * squareSize;
+    // If xPosition and yPosition are not defined, define them.
+    if (!this.xPosition && !this.yPosition) {
+      this.xPosition = this.column * squareSize;
+      this.yPosition = this.row * squareSize;
+      this.squarePreResize = squareSize;
+    }
 
-    ctx.drawImage(this.ladyPacImgs[this.ladyPacImgIndex], this.xPosition, this.yPosition, squareSize, squareSize)
+    ctx.drawImage(
+      this.ladyPacImgs[this.ladyPacImgIndex],
+      this.xPosition,
+      this.yPosition,
+      squareSize,
+      squareSize
+    );
   }
 
   /**
-   * Check which key was pressed.
+   * Check which key was pressed event.
+   * Set the position to be checked and if applicable set the move direction.
    */
 
   #checkKeyPressedEvent = (event) => {
     // Up key.
-    if(event.keyCode === 38) {
-      console.log('up')
+    if (event.keyCode === 38) {
+      if (this.moveDirection === MoveDirection.down) {
+        this.moveDirection = MoveDirection.up;
+      }
+      this.checkMoveDirection = MoveDirection.up;
     }
+
     // Down key.
-    if(event.keyCode === 40) {
-      console.log('down')
+    if (event.keyCode === 40) {
+      if (this.moveDirection === MoveDirection.up) {
+        this.moveDirection = MoveDirection.down;
+      }
+      this.checkMoveDirection = MoveDirection.down;
     }
+
     // Left key.
-    if(event.keyCode === 37) {
-      console.log('left')
+    if (event.keyCode === 37) {
+      if (this.moveDirection === MoveDirection.right) {
+        this.moveDirection = MoveDirection.left;
+      }
+      this.checkMoveDirection = MoveDirection.left;
     }
+
     // Right key.
-    if(event.keyCode === 39) {
-      console.log('right')
+    if (event.keyCode === 39) {
+      if (this.moveDirection === MoveDirection.left) {
+        this.moveDirection = MoveDirection.right;
+      }
+      this.checkMoveDirection = MoveDirection.right;
     }
-  }
+  };
 
   /**
    * Get all images and allow for their access.
@@ -71,10 +108,10 @@ export default class LadyPac {
 
   #getImages() {
     const img1 = new Image();
-    img1.src = "./assets/img/game/ladypac-open-half.png"
+    img1.src = "./assets/img/game/ladypac-open-half.png";
 
-    this.ladyPacImgs = [img1]
+    this.ladyPacImgs = [img1];
 
-    this.ladyPacImgIndex = 0
+    this.ladyPacImgIndex = 0;
   }
 }
